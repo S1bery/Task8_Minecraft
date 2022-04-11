7e086f18e95a45a05d435a39b1c906b9c35e15ea
S1bery <45617892+S1bery@users.noreply.github.com>
2022-04-11 21:48:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,43 +1,43 @@
 #include <iostream>
 
 int main() {
-    int landscape[5][5][10];
+    bool landscape[5][5][10];
 
     std::cout << "Enter the heights of blocks:" << std::endl;
     for(int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
-            for (int k = 0; k < 1; k++) {
-                std::cin >> landscape[i][j][k];
-                while (landscape[i][j][k] > 10 || landscape[i][j][k] < 0) {
-                    std::cout << "Error! Max height = 10, min = 0!" << std::endl;
-                    std::cout << "Enter the heights of blocks:" << std::endl;
-                    std::cin >> landscape[i][j][k];
+            int height;
+            std::cin >> height;
+            if (height < 0 || height > 10) {
+                std::cerr << "Invalid block height!" << std::endl; // как Я понял cerr используется для вывода непосредственно ошибок
+                return 0;
+            }
+            for (int k = 0; k < height; k++) {
+                landscape[i][j][k] = true;
                 }
+            for (int k = height; k < 10; k++) {
+                landscape[i][j][k] = false;
             }
         }
     }
 
-    int height = 0;
-    while (height != -1) {
-        std::cout << "Enter the height for horizontal slice:" << std::endl;
-        std::cin >> height;
-        while ((height > 10 || height < 0) && height != -1) {
-            std::cout << "Error! Max height = 10, min = 0!" << std::endl;
-            std::cout << "Enter the height for horizontal slice:" << std::endl;
-            std::cin >> height;
-        }
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                for (int k = 0; k < 1; k++) {
-                    if (landscape[i][j][k] >= height) {
-                        std::cout << "1" << " ";
-                    } else {
-                        std::cout << "0" << " ";
-                    }
-                }
+    int level;
+    std::cout << "Level:";
+    std::cin >> level;
+    if (level < 0 || level >= 10) {
+        std::cerr << "Invalid scan level!" << std::endl;
+        return 0;
+    }
+
+    for(int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (landscape[i][j][level]) {
+                std::cout << 1;
+            } else {
+                std::cout << 0;
             }
-            std::cout << std::endl;
         }
+        std::cout << std::endl;
     }
 
     return 0;
